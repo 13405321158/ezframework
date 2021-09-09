2ed01c91d1cc3abd48f8e189854de85b425b3b6c
weilai <weilai@139.com>
2021-09-09 17:13:07 +0800

    weilai

--- common-config/common-sql/src/main/java/com/leesky/ezframework/join/interfaces/one2one/One2One.java
+++ common-config/common-sql/src/main/java/com/leesky/ezframework/join/interfaces/one2one/One2One.java
@@ -8,14 +8,11 @@
 
 package com.leesky.ezframework.join.interfaces.one2one;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
+
 /**
  * 一对一映射
- * @author leesky
+ * 作者: 魏来
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
@@ -23,27 +20,30 @@ import java.lang.annotation.Target;
 public @interface One2One {
 
     /**
+     * <li> 实体类表名称
+     * <li> 另一个one方的数据表名称，注意是另一个one方，千万别写成当前one方的数据表名称
+     *
      * @作者: 魏来
      * @日期: 2021/8/21 上午10:46
-     * @描述: 实体类表名称
-     * @描述: 另一个one方的数据表名称，注意是另一个one方，千万别写成当前one方的数据表名称
      **/
     String otherOneTableName();
 
     /**
+     * <li> 依据此属性指定的字段去对方表中查询
+     * <li> 关联查询一般是主键，但也可以不是主键，所以用此属性指定
+     *
      * @作者: 魏来
-     * @日期: 2021/8/21 上午11:11
-     * @描述: 依据此属性指定的字段去对方表中查询
-     * @描述: 关联查询一般是主键，但也可以不是主键，所以用此属性指定
+     * @日期: 2021/8/21 上午10:46
      **/
     String otherOneTableColumn() default "id";
 
     /**
+     * <li> 标记映射关系 和当前bean哪个属性 关联
+     * <li> one2one 关系可以划分为主表 和 从表： 主表含有从表字段， 从表不含有主表字段
+     * <li> 此属性=“”代表从表;
+     *
      * @作者: 魏来
-     * @日期: 2021年8月24日 上午10:16:32
-     * @描述: 标记映射关系 和当前bean哪个属性 关联
-     * @描述: one2one 关系可以划分为主表 和 从表： 主表含有从表字段， 从表不含有主表字段
-     * @描述: 此属性=“”代表从表;
+     * @日期: 2021/8/21 上午10:46
      */
     String joinField() default "";
 
