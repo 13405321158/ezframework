66b2aa4842fac6d8a861f8ae6de0fb37e4a3d0bd
leesky <leesky09@13405321158.com>
2022-02-28 17:56:38 +0800

    weilai

--- base-config/authorization-server/src/main/java/com/leesky/ezframework/auth/utils/RequestUtils.java
+++ base-config/authorization-server/src/main/java/com/leesky/ezframework/auth/utils/RequestUtils.java
@@ -48,7 +48,7 @@ public class RequestUtils {
      * <p>
      * 兼容两种方式获取OAuth2客户端信息（client_id、client_secret）
      * 方式一：client_id、client_secret放在请求路径中
-     * 方式二：放在请求头（Request Headers）中的Authorization字段，且经过加密，例如 Basic Y2xpZW50OnNlY3JldA== 明文等于 client:secret
+     * 方式二：放在请求头（Request Headers）中的Authorization字段，且经过加密(BASE64算法)，例如 Basic Y2xpZW50OnNlY3JldA== （明文等于 client:secret)
      *
      * @return
      */
