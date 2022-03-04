71f4218c25f8f0b7d33f719a8caee42ce309e845
leesky09 <leesky09@13405321158.com>
2022-03-04 18:41:05 +0800

    weilai

---
+++ base-config/authorization-server/src/main/java/com/leesky/ezframework/auth/details/userdetails/CommonCode.java
@@ -0,0 +1,14 @@
+/*
+ * @作者: 魏来
+ * @日期: 2022/3/4 下午6:21
+ * @组织: 森麒麟轮胎股份有限公司.
+ * @部门: 国内市场替换部IT组
+ * @描述:
+ */
+package com.leesky.ezframework.auth.details.userdetails;    /**
+     * <li></li>
+     *
+     * @author: 魏来
+     * @date:  2022/3/4 下午6:21
+     */public class CommonCode {
+}
