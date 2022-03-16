049bdb81ac50f4e9faf7b5b39b4b17c6986499f7
leesky09 <leesky09@13405321158.com>
2022-03-16 17:32:26 +0800

    weilai

--- plugs-config/elasticsearch-server/src/main/java/com/leesky/ezframework/es/repo/Idem01Repo.java
+++
@@ -1,7 +0,0 @@
-package com.leesky.ezframework.es.repo;
-
-import com.leesky.ezframework.es.model.Demo01Model;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface Idem01Repo extends ElasticsearchRepository<Demo01Model, String> {
-}
--- plugs-config/elasticsearch-server/src/main/java/com/leesky/ezframework/es/repo/Idem02Repo.java
+++
@@ -1,7 +0,0 @@
-package com.leesky.ezframework.es.repo;
-
-import com.leesky.ezframework.es.model.Demo02Model;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface Idem02Repo extends ElasticsearchRepository<Demo02Model, String> {
-}
